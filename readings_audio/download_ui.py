fdbf43756229e2df6a349c2e1275bee721e60155
ch4noyu <ch4noyu@yahoo.com>
2016-03-13 13:52:46 +0900

    option to remove broken links

--- readings_audio/download_ui.py
+++ readings_audio/download_ui.py
@@ -21,16 +21,18 @@ import gui_download_design
 
 
 
-class MainGui(QtGui.QWidget, gui_download_design.Ui_Dialog):
+class MainGui(QtGui.QWidget, gui_download_design.Ui_crawler_dialog):
     def __init__(self):
         super(self.__class__, self).__init__()
         self.setupUi(self)
 
         # Names ...................
 
+        self.top_level_boxes = [self.checkBox_add_media, self.checkBox_download_missing]
         self.add_dependant_boxes = [self.radioButton_extend, self.radioButton_overwrite,
-                                    self.radioButton_overwrite_empty]
+                                    self.radioButton_overwrite_empty, self.checkBox_remove_broken]
         self.download_dependant_boxes = [self.checkBox_ignore_blacklist, self.checkBox_redownload]
+        self.all_boxes = self.top_level_boxes + self.add_dependant_boxes + self.download_dependant_boxes
 
         # Setup values ..................
 
@@ -40,19 +42,20 @@ class MainGui(QtGui.QWidget, gui_download_design.Ui_Dialog):
 
         # just to be sure ..................
 
-        self.box_implications_add()
-        self.box_implications_download()
+        self.box_implications()
 
         # Connections ..................
 
-        self.checkBox_download_missing.stateChanged.connect(self.box_implications_download)
-        self.checkBox_add_media.stateChanged.connect(self.box_implications_add)
+        for box in self.all_boxes:
+            box.clicked.connect(self.box_implications)
+
         self.pushButton_start.clicked.connect(self.mock_loop)
 
     def mock_loop(self):
         self.label_status.setText("Status: Running...")
 
         self.progressBar.setEnabled(True)
+        self.tableWidget.setEnabled(True)
 
         self.pushButton_stop.setEnabled(True)
         self.pushButton_quit.setEnabled(False)
@@ -61,13 +64,13 @@ class MainGui(QtGui.QWidget, gui_download_design.Ui_Dialog):
         self.progressBar.setMaximum(100)
         for i in range(100):
             self.label_status.setText("Status: {}".format(i))
-            self.progressBar    .setValue(i)
-            time.sleep(1)
+            self.progressBar.setValue(i)
             item = QtGui.QTableWidgetItem(str(i))
             item.setTextAlignment(QtCore.Qt.AlignRight)
             self.tableWidget.setItem(2,2, item)
             if i == 3:
                 break
+            time.sleep(1)
         self.label_status.setText("Status: Idle...")
 
         self.pushButton_stop.setEnabled(False)
@@ -85,11 +88,15 @@ class MainGui(QtGui.QWidget, gui_download_design.Ui_Dialog):
         self.progressBar.setEnabled(False)
         self.progressBar.reset()
 
-    def box_implications_add(self):
+    def box_implications(self):
         for radio_button in self.add_dependant_boxes:
             radio_button.setEnabled(self.checkBox_add_media.isChecked())
+        if self.radioButton_extend.isChecked():
+            self.checkBox_remove_broken.setEnabled(True)
+        else:
+            self.checkBox_remove_broken.setChecked(True)
+            self.checkBox_remove_broken.setEnabled(False)
 
-    def box_implications_download(self):
         for check_box in self.download_dependant_boxes:
             check_box.setEnabled(self.checkBox_download_missing.isChecked())
 
